a70add0f05882fa74bfab4f7c350e35b88c87fec
syair4 <syair4@gmail.com>
2020-01-28 13:37:02 +0200

    Update ApiReq.js

--- js/ApiReq.js
+++ js/ApiReq.js
@@ -1,19 +1,19 @@
   const apiKey = '9NX3Oqwv5lZ41dHba9boDdOMZ7HA1uTp';
   
   function currentConditionsReq (respFunc,city) {
-    var url = "http://dataservice.accuweather.com/currentconditions/v1/locationKey?apikey="
+    var url = "https://dataservice.accuweather.com/currentconditions/v1/locationKey?apikey="
     +apiKey+"&locationKey="+city.id;
   getReq(url,respFunc,city);
   }
 
   function fiveDayReq (respFunc,city) {
-    var url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey?apikey="
+    var url = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey?apikey="
     +apiKey+"&locationKey="+city.id+"&metric=true&details=true";
    getReq(url,respFunc,city);
   }
 
   function autocompleteReq (str,respondFunc) {
-    var url = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="
+    var url = "https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="
     +apiKey+"&q="+str+"&language=en-us";
     getReq(url,respondFunc);
   }
